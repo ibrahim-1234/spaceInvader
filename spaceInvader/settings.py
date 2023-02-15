6ccc63f292e6e88888d6b6484b4722e82b8c99d4
ibrahim-1234 <aabr333@hotmail.com>
2023-02-16 02:12:06 +0300

    a

--- spaceInvader/settings.py
+++ spaceInvader/settings.py
@@ -148,8 +148,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'brhoome74@gmail.com' 
 EMAIL_HOST_PASSWORD = 'zpwcgicfskvipbrc'
 
